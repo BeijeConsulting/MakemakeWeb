8431ffa4e82a37b3bfba7ac42f46d3c2928dc523
tyra <tyramariedeclaro@gmail.com>
2021-05-10 16:24:51 +0200

    merge

--- src/it/beije/makemake/web/rubrica/ServletRubrica.java
+++ src/it/beije/makemake/web/rubrica/ServletRubrica.java
@@ -28,8 +28,8 @@ public class ServletRubrica extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		String operazione= null;
-		operazione= request.getParameter(operazione);
+		
+		String operazione= request.getParameter("operazione");
 		System.out.println(operazione);
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
