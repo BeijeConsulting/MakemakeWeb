c4460f87ce8c74462c0479a7696f3935dca641b0
AntonyShenouda <antonyshenouda@outlook.it>
2021-05-10 16:30:14 +0200

    Resolve conflicts

--- src/it/beije/makemake/web/DatabaseReader.java
+++ src/it/beije/makemake/web/DatabaseReader.java
@@ -42,7 +42,6 @@ public class DatabaseReader extends HttpServlet {
 		PrintWriter printer = response.getWriter();
 		printer.append(htmlStart).append("<p>");
 		
-<<<<<<< HEAD:src/it/beije/makemake/web/DatabaseReader.java
 		switch(value) {
 			case "select":
 				List<Contatto> contList = JpaContactUtil.selectAll();
@@ -55,56 +54,13 @@ public class DatabaseReader extends HttpServlet {
 			case "insert":
 				break;
 		}
-=======
-//		response.getWriter().append(htmlStart).append("<h1>Sono un Titolo</h1><p>questa è una prova</p><br>")
-//		.append("<form action=\"test\" method=\"post\">\r\n" + 
-//				"		  <label for=\"fname\">First name:</label><br>\r\n" + 
-//				"		  <input type=\"text\" name=\"fname\"><br>\r\n" + 
-//				"		  <label for=\"lname\">Last name:</label><br>\r\n" + 
-//				"		  <input type=\"text\" name=\"lname\"><br><br>\r\n" + 
-//				"		  <input type=\"submit\" value=\"Submit\">\r\n" + 
-//				"		</form>")
-//		.append(htmlEnd);
-		
-		
-		HttpSession session = request.getSession();
-		
-		String fname = (String) session.getAttribute("fname");
-		String lname = (String) session.getAttribute("lname");
-		
-		response.getWriter().append(htmlStart)
-		.append("fname : ").append(fname).append("<br>")
-		.append("lname : ").append(lname)
-		.append(htmlEnd);
->>>>>>> origin:src/it/beije/makemake/web/TestServlet.java
+
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-<<<<<<< HEAD:src/it/beije/makemake/web/DatabaseReader.java
-=======
-		System.out.println("test POST");
-		
-		String fname = request.getParameter("fname");
-		String lname = request.getParameter("lname");
-		System.out.println("fname : " + fname);
-		System.out.println("lname : " + lname);
-		
-		//..... elaborazione
-		// accedere al DB
-		// ricerco su rubrica un contatto con  fname & lname
-		
-		HttpSession session = request.getSession();
-		
-		session.setAttribute("fname", fname);
-		session.setAttribute("lname", lname);
-		
-		
-		response.getWriter().append(htmlStart)
-		.append("fname : ").append(fname).append("<br>").append("lname : ").append(lname)
-		.append(htmlEnd);
->>>>>>> origin:src/it/beije/makemake/web/TestServlet.java
+
 	}
 }
